8c9a0b7c69619a6e5e6fa644178a0667488a3355
JinseongHwang <eddy5360@naver.com>
2020-12-01 03:54:23 +0900

    feat: make scroll bar available

---
+++ src/boothalgorithm2/BoothFormV2.java
@@ -0,0 +1,107 @@
+package boothalgorithm2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class BoothFormV2 {
+    public static int formBit = 0;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Booth Algorithm GUI - 1723940 황진성");
+        JPanel mainPanel = new JPanel();
+        JRadioButton bit4Btn = new JRadioButton("4bit");
+        JRadioButton bit8Btn = new JRadioButton("8bit");
+        JRadioButton bit16Btn = new JRadioButton("16bit");
+        JRadioButton bit32Btn = new JRadioButton("32bit");
+        JRadioButton bit64Btn = new JRadioButton("64bit");
+        JTextArea resultArea = new JTextArea(30, 100);
+        JTextField multiplicandInputField = new JTextField(30);
+        JTextField multiplierInputField = new JTextField(30);
+        JButton executeBtn = new JButton("실행");
+        JButton exitBtn = new JButton("종료");
+        JLabel noticeLabel = new JLabel("반드시 bit, 피승수, 승수를 입력 후 실행 버튼을 눌려주세요!");
+        JLabel multiplicandLabel = new JLabel("피승수 입력: ");
+        JLabel multiplierLabel = new JLabel("승수 입력");
+        JScrollPane scroll = new JScrollPane(resultArea);
+
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        resultArea.setFont(new Font("D2Coding ligature", Font.BOLD, 15));
+
+        // ------------------------------------------ Event Listener - START
+
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (bit4Btn.isSelected()) {
+                    formBit = 4;
+                } else if (bit8Btn.isSelected()) {
+                    formBit = 8;
+                } else if (bit16Btn.isSelected()) {
+                    formBit = 16;
+                } else if (bit32Btn.isSelected()) {
+                    formBit = 32;
+                } else if (bit64Btn.isSelected()) {
+                    formBit = 64;
+                }
+            }
+        };
+        bit8Btn.addActionListener(listener);
+        bit16Btn.addActionListener(listener);
+        bit32Btn.addActionListener(listener);
+        bit64Btn.addActionListener(listener);
+        bit4Btn.addActionListener(listener);
+
+        executeBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BoothV2 booth = new BoothV2(formBit, multiplicandInputField.getText(), multiplierInputField.getText());
+                ArrayList<String> paramResult = booth.writeInResultArea();
+                for (String elem : paramResult) {
+                    resultArea.append(elem + "\n");
+                }
+            }
+        });
+
+        exitBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        // ------------------------------------------ Event Listener - END
+
+        mainPanel.add(noticeLabel);
+
+        mainPanel.add(bit4Btn);
+        mainPanel.add(bit8Btn);
+        mainPanel.add(bit16Btn);
+        mainPanel.add(bit32Btn);
+        mainPanel.add(bit64Btn);
+
+        mainPanel.add(multiplicandLabel);
+        mainPanel.add(multiplicandInputField);
+
+        mainPanel.add(multiplierLabel);
+        mainPanel.add(multiplierInputField);
+
+        mainPanel.add(scroll);
+        mainPanel.add(executeBtn);
+        mainPanel.add(exitBtn);
+
+        frame.setContentPane(mainPanel);
+
+        // ------------------------------------------ vital functions for GUI program - START
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(1500, 650));
+        frame.pack();
+        frame.setVisible(true);
+        // ------------------------------------------ vital functions for GUI program - END
+    }
+
+}
---
+++ src/boothalgorithm2/BoothV2.java
@@ -0,0 +1,273 @@
+package boothalgorithm2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BoothV2 {
+
+    public int bit = 0;
+    public int operateStartIndex = 0;
+    public ArrayList<Long> multiplicand = new ArrayList<>();
+    public ArrayList<Long> multiplier = new ArrayList<>();
+    public ArrayList<Long> result = new ArrayList<>();
+    public ArrayList<Long> multiplicandComplement;
+    public ArrayList<String> resultList = new ArrayList<>();
+
+    StringBuilder currLine = new StringBuilder();
+
+    public BoothV2(int bit, String multiplicandInput, String multiplierInput) {
+        this.bit = bit;
+        getUserInput(multiplicandInput, multiplierInput);
+        initResult();
+        printBinaryOperand();
+        startOperation();
+        printDecimalResult();
+    }
+
+    public ArrayList<String> writeInResultArea() {
+        return resultList;
+    }
+
+    public void writeStr(String str) {
+        resultList.add(str);
+    }
+
+//    public double log2(double x) {
+//        return Math.log(x) / Math.log(2);
+//    }
+
+    //public void writeArr(ArrayList<Long> arr, int startIndex) {
+    public void writeArr(int type, int startIndex) {
+        //currLine.append("Multiplier     => ");
+        if (type == 1) {
+            currLine.append("Add Operation  => ");
+        } else if (type == 2) {
+            currLine.append("Sub Operation  => ");
+        } else {
+            currLine.append("Only Shift     => ");
+        }
+        for (int i = 0; i < startIndex + (bit / 4) - 1; i++) {
+            currLine.append(" ");
+        }
+        for (int i = startIndex; i < startIndex + bit; i++) {
+            //currLine.append(arr.get(i));
+            if (type < 3) {
+                currLine.append(multiplicand.get(i));
+            } else {
+                currLine.append("0");
+            }
+            if (i > 0 && (i + 1) % 4 == 0) {
+                currLine.append(" ");
+            }
+        }
+        resultList.add(currLine.toString());
+        currLine.setLength(0);
+    }
+
+    public void getUserInput(String multiplicandInput, String multiplierInput) {
+        long m1 = Long.parseLong(multiplicandInput);
+        convertToBinary(true, Math.abs(m1));
+        while (multiplicand.size() < 2 * bit) {
+            multiplicand.add((long)0);
+        }
+        if (m1 < 0) {
+            convertToComplement(multiplicand);
+        }
+        Collections.reverse(multiplicand);
+
+        long m2 = Long.parseLong(multiplierInput);
+        convertToBinary(false, Math.abs(m2));
+        while (multiplier.size() < bit) {
+            multiplier.add((long)0);
+        }
+        if (m2 < 0) {
+            convertToComplement(multiplier);
+        }
+        Collections.reverse(multiplier);
+    }
+
+    public void convertToBinary(boolean selector, long d) {
+        while (d > 0) {
+            long currentRest = d % 2;
+            if (selector) {
+                multiplicand.add(currentRest);
+            } else {
+                multiplier.add(currentRest);
+            }
+            d /= 2;
+        }
+    }
+
+    public void convertToComplement(ArrayList<Long> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) == 0) {
+                arr.set(i, (long) 1);
+            } else {
+                arr.set(i, (long) 0);
+            }
+        }
+        int firstZeroIndex = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) == 0) {
+                firstZeroIndex = i;
+                arr.set(i, (long) 1);
+                break;
+            }
+        }
+        for (int i = 0; i < firstZeroIndex; i++) {
+            arr.set(i, (long) 0);
+        }
+    }
+
+    public void initResult() {
+        operateStartIndex = bit * 2 - 1;
+        for (int i = 0; i < bit * 2; i++) {
+            result.add((long) 0);
+        }
+    }
+
+    public void shiftLeft() {
+        operateStartIndex--;
+    }
+
+    public void addOperation(ArrayList<Long> operand) {
+        boolean carry = false;
+        for (int resultIndex = operateStartIndex, multiplicandIndex = operand.size() - 1;
+             resultIndex >= 0;
+             resultIndex--, multiplicandIndex--) {
+            long currResultBit = result.get(resultIndex);
+            long currMultiplicandBit = operand.get(multiplicandIndex);
+            if (carry) {
+                if (currResultBit + currMultiplicandBit == 2) {
+                    result.set(resultIndex, (long)1);
+                    carry = true;
+                } else if (currResultBit + currMultiplicandBit == 1) {
+                    result.set(resultIndex, (long)0);
+                    carry = true;
+                } else {
+                    result.set(resultIndex, (long)1);
+                    carry = false;
+                }
+            } else {
+                if (currResultBit + currMultiplicandBit == 2) {
+                    result.set(resultIndex, (long)0);
+                    carry = true;
+                } else if (currResultBit + currMultiplicandBit == 1) {
+                    result.set(resultIndex, (long)1);
+                    carry = false;
+                } else {
+                    result.set(resultIndex, (long)0);
+                    carry = false;
+                }
+            }
+        }
+    }
+
+    public void startOperation() {
+        multiplicandComplement = new ArrayList<>(multiplicand);
+        Collections.reverse(multiplicandComplement);
+        convertToComplement(multiplicandComplement);
+        Collections.reverse(multiplicandComplement);
+        multiplier.add((long)0);
+
+        for (int i = multiplier.size() - 1; i > 0; i--) {
+            long preBit = multiplier.get(i - 1);
+            long postBit = multiplier.get(i);
+
+            printBinaryResult();
+
+            if (preBit == 0 && postBit == 1) {
+                writeArr(1, operateStartIndex - bit + 1);
+                addOperation(multiplicand);
+            } else if (preBit == 1 && postBit == 0) {
+                writeArr(2, operateStartIndex - bit + 1);
+                addOperation(multiplicandComplement);
+            } else {
+                writeArr(3, operateStartIndex - bit + 1);
+            }
+            shiftLeft();
+            printLine();
+        }
+    }
+
+    public long binaryToDecimal(ArrayList<Long> bin) {
+        long sum = 0;
+        boolean isNegative = false;
+        if (bin.get(0) == 1) {
+            isNegative = true;
+            // 2의 보수로 바꿔줌 - START
+            Collections.reverse(bin);
+            convertToComplement(bin);
+            Collections.reverse(bin);
+            // 2의 보수로 바꿔줌 - END
+        }
+        for (int i = 0; i < bin.size(); i++) {
+            if (bin.get(bin.size() - i - 1) == 1) {
+                sum += (Math.pow(2, i));
+            }
+        }
+        if (isNegative) return -sum;
+        else return sum;
+    }
+
+    public void printLine() {
+        writeStr("----------------------------------------------------------------------------------------" +
+                "----------------------------------------------------------------------------------------");
+    }
+
+    public void printBinaryOperand() {
+        currLine.append("Multiplicand   => ");
+        //System.out.print("Multiplicand   => ");
+        for (int i = bit; i < multiplicand.size(); i++) {
+            currLine.append(multiplicand.get(i));
+            //System.out.print(multiplicand.get(i));
+            if (i > 0 && (i + 1) % 4 == 0) {
+                currLine.append(" ");
+                //System.out.print(" ");
+            }
+        }
+        //System.out.println();
+        writeStr(currLine.toString());
+        currLine.setLength(0);
+
+        currLine.append("Multiplier     => ");
+        //System.out.print("Multiplier     => ");
+        for (int i = 0; i < multiplier.size(); i++) {
+            currLine.append(multiplier.get(i));
+            //System.out.print(multiplier.get(i));
+            if (i > 0 && (i + 1) % 4 == 0) {
+                currLine.append(" ");
+                //System.out.print(" ");
+            }
+        }
+        //System.out.println();
+        writeStr(currLine.toString());
+        currLine.setLength(0);
+        printLine();
+    }
+
+    public void printBinaryResult() {
+        currLine.append("Result Binary  => ");
+        //System.out.print("Result Binary  => ");
+        for (int i = 0; i < result.size(); i++) {
+            currLine.append(result.get(i));
+            //System.out.print(result.get(i));
+            if (i > 0 && (i + 1) % 4 == 0) {
+                currLine.append(" ");
+                //System.out.print(" ");
+            }
+        }
+        //System.out.println();
+        writeStr(currLine.toString());
+        currLine.setLength(0);
+    }
+
+    public void printDecimalResult() {
+        currLine.append("Result Decimal => ");
+        currLine.append(Long.toString(binaryToDecimal(result)));
+        //System.out.println("Result Decimal => " + binaryToDecimal(result));
+        writeStr(currLine.toString());
+        currLine.setLength(0);
+        printLine();
+    }
+}
